3b1d3668865ef16af106d4a0af6055ef66cf5e79
Bharathkumaryuva <109011816+Bharathkumaryuva@users.noreply.github.com>
2022-12-18 11:57:23 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,21 @@
+/*document.write("hello");
+console.log("hi");
+alert("Are you ok");
+confirm("choose me!");
+prompt("enter a value");*/
+
+var a = 5;
+var b = "Naga";
+var c = null;
+var d = undefined;
+var e = true;
+var f = " ";
+var g;
+var h = 10;
+
+/*document.write(a+"_"+b+"_"+c);
+console.log(g);
+alert(b+h+a);*/
+
+
+/*console.log(num1+num2);*/
